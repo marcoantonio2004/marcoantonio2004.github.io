e5b55bf47b04724558115f3fd903c13a074b496a
marcoantonio <marooagc04@gmail.com>
2025-11-16 21:18:34 -0600

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://marcoantonio2004.github.io/marcoagc.github.io',
-  base: '/marcoagc.github.io',
-
+  site: 'https://marcoantonio2004.github.io',
+  base: '/',
   integrations: [
     tailwind({
       config: {
